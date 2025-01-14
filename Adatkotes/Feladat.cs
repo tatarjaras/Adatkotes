34a8125c035aff435fd2f1fa2b56c620d646b2d7
tatarjaras <havasim@kkszki.hu>
2025-01-14 11:56:57 +0100

    Add project files.

---
+++ Adatkotes/Feladat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adatkotes
+{
+    internal class Feladat
+    {
+        public  int FeladatId { get; set; }
+        public string? Leiras { get; set; }
+        public DateTime Hatarido { get; set; }
+        public bool Kesz { get; set;}
+        public int Prioritas { get;set; }
+    }
+}
